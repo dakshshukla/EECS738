dc32f0fa23661519c006d9d5b834ae8840641b76
Daksh Shukla <dakshshukla@gmail.com>
2019-02-19 21:31:49 -0600

    Modified EM_GMM code to incorporate multiple epochs for different initial guesses of means | Added PNG files for some results

--- EM_GMM.py
+++ EM_GMM.py
@@ -6,8 +6,8 @@ Created on Sun Feb 17 21:14:02 2019
 """
 
 # Command for runnning via IPython console in Spyder:
-# runfile('EM_GMM.py', args = 'winequality-red.csv 1 5')
-# runfile('EM_GMM.py', args = 'iris.csv 1 5')
+# runfile('EM_GMM.py', args = 'winequality-red.csv 1 5 50')
+# runfile('EM_GMM.py', args = 'iris.csv 2 5 50')
 
 import pandas as pd
 import sys
@@ -20,18 +20,21 @@ from numpy.linalg import inv, det
 #data_select = np.reshape(data_select, (20,1))
 #data_select = np.sort(data_select, axis = 0)
 #fname = 'winequality-red.csv'
-#feature_num = 4
-#max_K = 3
+#fname = 'iris.csv'
+#feature_num = 1
+#max_K = 5
+#max_epoch = 100
 """ For testing only end"""
 
 fname = sys.argv[1]
 feature_num = int(sys.argv[2])
 max_K = int(sys.argv[3])
+max_epoch = int(sys.argv[4])
 
 fdata = pd.read_csv(fname)
 data_col = fdata.get_values()
 data_select = np.reshape(data_col[:,feature_num-1], (np.size(data_col,0),1))
-data_select = np.sort(data_select, axis = 0)
+data_select = np.sort(data_select.astype(float), axis = 0)
 
 eps_likelihood = 1e-3
 likelihood_del = 1
@@ -46,79 +49,105 @@ def gauss_fun(x, mu, var):
 def em(max_K):
     for K in range (2, max_K+1):
         print('Trying #Gaussians or K =', K)
-        for mu_iter in range(0,K):
-            idx = np.random.choice(data_select.shape[0], 1, replace=False)
-            if mu_iter == 0:
-                idx_all = idx
-            else:
-                idx_all = np.append(idx_all, idx)
-        mu_hat = np.full((K,1), data_select[idx_all])
-        var_hat = np.full((K,1), np.var(data_select))
-        pi_hat = np.full((K,1), 1/K)
-        gamma_hat = np.empty([np.size(data_select),K])
-        iteration = 0
-        likelihood_del = 1
-        while(likelihood_del > eps_likelihood) and (iteration < 50):
-            for k in range(0,K):
-                for n in range(0,np.size(data_select)):
-                    # Expectation Step
-                    numera_tor = (pi_hat[k,0])*(gauss_fun(data_select[n,0], mu_hat[k,0], var_hat[k,0]))
-                    denomina_tor = np.sum((pi_hat[:,0])*(gauss_fun(data_select[n,0], mu_hat[:,0], var_hat[:,0])))
-                    gamma_hat[n,k] = numera_tor/denomina_tor
-                # Maximization Step
-                N_k = np.sum(gamma_hat[:,k])
-                mu_hat[k,0] = 0
-                for n in range(0,np.size(data_select)):
-                    mu_hat[k,0] = mu_hat[k,0] + (gamma_hat[n,k]*data_select[n,0])
-                mu_hat[k,0] = (1/N_k)*mu_hat[k,0]
-                var_hat[k,0] = 0
-                for n in range(0,np.size(data_select)):
-                    var_hat[k,0] = var_hat[k,0] + (gamma_hat[n,k]*((data_select[n,0] - mu_hat[k,0])*(data_select[n,0] - mu_hat[k,0])))
-                var_hat[k,0] = (1/N_k)*var_hat[k,0]
-                pi_hat[k,0] = N_k/np.size(data_select)
-                if iteration == 0:
-                    pi_hat_all = pi_hat
+        epoch = 0
+        while epoch < max_epoch:
+            for mu_iter in range(0,K):
+                idx = np.random.choice(data_select.shape[0], 1, replace=False)
+                if mu_iter == 0:
+                    idx_all = idx
                 else:
-                    pi_hat_all = np.hstack((pi_hat_all, pi_hat))
-            likelihood = 0
-            for n in range(0,np.size(data_select)):
-                likelihood = likelihood + np.log(np.sum(pi_hat[:,0]*gauss_fun(data_select[n,0], mu_hat[:,0], var_hat[:,0])))
-            if iteration == 0:
-                likelihood_all = likelihood
+                    idx_all = np.append(idx_all, idx)
+            mu_hat = np.full((K,1), data_select[idx_all])
+            var_hat = np.full((K,1), np.var(data_select))
+            pi_hat = np.full((K,1), 1/K)
+            gamma_hat = np.empty([np.size(data_select),K])
+            iteration = 0
+            likelihood_del = 1
+            while(likelihood_del > eps_likelihood) and (iteration < 50):
+                for k in range(0,K):
+                    for n in range(0,np.size(data_select)):
+                        # Expectation Step
+                        numera_tor = (pi_hat[k,0])*(gauss_fun(data_select[n,0], mu_hat[k,0], var_hat[k,0]))
+                        denomina_tor = np.sum((pi_hat[:,0])*(gauss_fun(data_select[n,0], mu_hat[:,0], var_hat[:,0])))
+                        gamma_hat[n,k] = numera_tor/denomina_tor
+                    # Maximization Step
+                    N_k = np.sum(gamma_hat[:,k])
+                    mu_hat[k,0] = 0
+                    for n in range(0,np.size(data_select)):
+                        mu_hat[k,0] = mu_hat[k,0] + (gamma_hat[n,k]*data_select[n,0])
+                    mu_hat[k,0] = (1/N_k)*mu_hat[k,0]
+                    var_hat[k,0] = 0
+                    for n in range(0,np.size(data_select)):
+                        var_hat[k,0] = var_hat[k,0] + (gamma_hat[n,k]*((data_select[n,0] - mu_hat[k,0])*(data_select[n,0] - mu_hat[k,0])))
+                    var_hat[k,0] = (1/N_k)*var_hat[k,0]
+                    pi_hat[k,0] = N_k/np.size(data_select)
+                    if iteration == 0:
+                        pi_hat_all = pi_hat
+                    else:
+                        pi_hat_all = np.hstack((pi_hat_all, pi_hat))
+                likelihood = 0
+                for n in range(0,np.size(data_select)):
+                    likelihood = likelihood + np.log(np.sum(pi_hat[:,0]*gauss_fun(data_select[n,0], mu_hat[:,0], var_hat[:,0])))
+                if iteration == 0:
+                    likelihood_iteration = likelihood
+                else:
+                    likelihood_iteration = np.append(likelihood_iteration, likelihood)
+                if iteration > 0:
+                    likelihood_del = np.abs(likelihood_iteration[-1]) - np.abs(likelihood_iteration[-2])
+                iteration = iteration + 1
+            if epoch == 0:
+                likelihood_epoch = likelihood_iteration[-1]
+                mu_hat_epoch = mu_hat
+                var_hat_epoch = var_hat
+                pi_hat_epoch = pi_hat
             else:
-                likelihood_all = np.append(likelihood_all, likelihood)
-            if iteration > 0:
-                likelihood_del = np.abs(likelihood_all[-1]) - np.abs(likelihood_all[-2])
-            iteration = iteration + 1
+                likelihood_epoch = np.append(likelihood_epoch, likelihood_iteration[-1])
+                mu_hat_epoch = np.hstack((mu_hat_epoch, mu_hat))
+                var_hat_epoch = np.hstack((var_hat_epoch, var_hat))
+                pi_hat_epoch = np.hstack((pi_hat_epoch, pi_hat))
+            epoch = epoch + 1
+        best_likelihood_epoch_idx = np.argmax(likelihood_epoch)
         if K == 2:
-            likelihood_K = likelihood_all[-1]
+            likelihood_K = likelihood_epoch[best_likelihood_epoch_idx]
+            mu_hat_K = mu_hat_epoch[:,best_likelihood_epoch_idx]
+            var_hat_K = var_hat_epoch[:,best_likelihood_epoch_idx]
+            pi_hat_K = pi_hat_epoch[:,best_likelihood_epoch_idx]
         else:
-            likelihood_K = np.append(likelihood_K, likelihood_all[-1])
+            likelihood_K = np.append(likelihood_K, likelihood_epoch[best_likelihood_epoch_idx])
+            mu_hat_K = np.append(mu_hat_K, mu_hat_epoch[:,best_likelihood_epoch_idx])
+            var_hat_K = np.append(var_hat_K, var_hat_epoch[:,best_likelihood_epoch_idx])
+            pi_hat_K = np.append(pi_hat_K, pi_hat_epoch[:,best_likelihood_epoch_idx])
         K = K + 1
-    return likelihood_K, mu_hat, var_hat, pi_hat
+    return likelihood_K, mu_hat_K, var_hat_K, pi_hat_K
 
-likelihood_K, mu_hat, var_hat, pi_hat = em(max_K)
-best_likeli_idx = np.argmax(likelihood_K)
-K = best_likeli_idx + 2
+likelihood_K, mu_hat_K, var_hat_K, pi_hat_K = em(max_K)
+best_likelihood_K_idx = np.argmax(likelihood_K)
+K = best_likelihood_K_idx + 2
 print('Selecting Number of Gaussians =', K)
-likelihood_K_sel, mu_hat, var_hat, pi_hat = em(K)
+start_idx = int(np.sum(range(2,K)))
+mu_hat = (mu_hat_K[start_idx:start_idx+K]).reshape(K,1)
+var_hat = (var_hat_K[start_idx:start_idx+K]).reshape(K,1)
+pi_hat = (pi_hat_K[start_idx:start_idx+K]).reshape(K,1)
 
-pd = np.full((data_select.shape[0], K-1), float(0))
+pd = np.full((data_select.shape[0], K), float(0))
 pd_mix = 0
 
-for k in range(0,K-1):
+for k in range(0,K):
     for n in range(0,data_select.shape[0]):
          x = gauss_fun(data_select[n,0], mu_hat[k,0], var_hat[k,0])
          pd[n,k] = x
     pd_mix = pd_mix + pi_hat[k,0]*pd[:,k]
 
-weights = np.ones_like(data_select[:,0])/data_select.shape[0]
+plt.rcParams.update({'font.size': 20})
 
 plt.figure(1)
 plt.plot(data_select[:,0], pd_mix, 'r', label = 'pdf estimate')
 plt.hist(data_select[:,0], density = True, label = 'True Data')
 plt.legend()
-plt.title('Number of Gaussians = %i' %K)
+plt.xlabel('Data')
+plt.ylabel('PDF')
+plt.title('True data compared to Estimated pdf, Number of Gaussians = %i' %K)
+plt.rcParams['lines.linewidth'] = 3
 plt.show
 
 plt.figure(2)
--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 This is Daksh Shukla's repository for EECS 738 course (Spring 2019) at The university of Kansas.
 
 Project 1:
-The technical parts of the project were discussed with Mr. Ben Liu who helped with understanding the fundamentals and formulate ideas to carry out the project.
+Some results of some selected features from particular datasets are shown as PNG images.
+Example: "feature_1_winquality_like" is the result of EM GMM on column number 1 data from
+winequality-red.csv file, and it shows the maximum likelihood with respect to number of Gaussians.
+"feature_1_winequality_pdf"  is the result of EM GMM on column number 1 data from
+winequality-red.csv file, and it shows the true data histogram and the estimated mixed
+probability density function.
+
+Some technical parts of the project were discussed with Mr. Ben Liu who helped with
+understanding the fundamentals and formulate ideas to carry out the project.
 
 File 1: EM_GMM.py - Main code for Expectation Maximization (EM) algorithm for Gaussian Mixture Models (GMM).
 The code is based on section 9.2.2 of Pattern Recognition and Machine Learning by Christopher M. Bishop.
@@ -10,18 +18,25 @@ The code is based on section 9.2.2 of Pattern Recognition and Machine Learning b
 1. Running the code: The code takes in 3 arguments
 	a) <filename>.csv
 	b) Column number of the csv file that is the feature
-	c) Maximum number of Gaussians to try out
-Example: 'python3 winequality-red.csv 1 5'
+	c) Maximum number of Gaussians (K) to try out (max_K)
+	d) Maximum epochs or iterations for different initial guesses of means for a particular K (max_epoch)
+Example: 'python3 winequality-red.csv 1 5 50'
+Caution: for large datasets higher numbers of max_epoch (>50) and higher numbers for max_K (>5) can cause slow processing
 
-2. The code computes the means and variances for the respective number of Gaussians (denoted by capital K)
+2. The code computes the means and variances for the respective number of Gaussians (denoted by capital K),
+and it does this max_epoch times while choosing different means as the starting point in the EM algorithm
 
-3. Then it computes the log likelihood for this particular mixture
+3. Then it computes the log likelihood for this particular mixture for max_epoch different initial conditions and
+selects the epoch with best (highest) maximum likelihood.
 
-4. It increments the K value (number of Gaussians) up to the maximum K input and re-computes means and variances and the corresponding log-likelihood
+4. It increments the K value (number of Gaussians) up to the maximum K input and re-computes means and variances and
+the corresponding log-likelihood (goes back to step 2)
 
-5. After going through all K's, it compares and selects the best Gaussian mixture K value
+5. After going through all K's, it compares and selects the best Gaussian mixture K value based on best (highest)
+maximum likelihood value
 
-6. Finally it computes the mixture pdf based on these parameters and plots and compares the estimated pdf with the true data
+6. Finally it computes the mixture pdf based on these selected parameters and plots and compares the
+estimated pdf with the true data
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -29,7 +44,7 @@ File 2: em_esl8_5.py - Code for practicing and understanding the EM algorithm fo
 This code is based on section 8.5 of book The Elements of Statistical Learning by Trevor Hastie, Robert Tibshirani and Jerome Friedman.
 
 1. Running the code: The code does not take any arguments and runs simply as 'python3 em_esl8_5.py'
-It already has the data vector from the book's section hard-coded in a numoy array.
+It already has the data vector from the book's section hard-coded in a numpyy array.
 
 2. It computes means and variances for a mixture of 2 Gaussians and iterates 300 times through selection of
 different initial guesses of means.
