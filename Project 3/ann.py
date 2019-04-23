3d30654b2af3d67382959a23fddfc5cab64c0d17
Daksh Shukla <dakshshukla@gmail.com>
2019-04-22 21:46:52 -0500

    Added project 3

---
+++ Project 3/ann.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 15:51:50 2019
+
+@author: daksh
+"""
+import numpy as np
+
+def sigmoid(x):
+    return 1.0/(1+ np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1.0 - x)
+
+class NeuralNetwork:
+    def __init__(self, x, y):
+        self.input      = x
+        self.weights1   = np.random.rand(self.input.shape[1],4)
+        #self.bias1 = np.random.rand(self.input.shape[0])
+        self.weights2   = np.random.rand(self.weights1.shape[1],1)
+        self.y          = y
+        self.output     = np.zeros(y.shape)
+        
+    def feedforward(self):
+        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+        self.output = sigmoid(np.dot(self.layer1, self.weights2))
+        
+    def backprop(self):
+        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
+        d_weights2 = np.dot(self.layer1.T, (-2*(self.y - self.output) * sigmoid_derivative(self.output)))
+        d_weights1 = np.dot(self.input.T,  (np.dot(-2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
+        #d_bias1 = -2*(self.y - self.output) * sigmoid_derivative(self.output)
+        # update the weights with the derivative (slope) of the loss function
+        self.weights1 -= d_weights1
+        self.weights2 -= d_weights2
+        
+        return self.weights1, self.weights2
+
+if __name__ == "__main__":
+    X = np.array([[0,0,1],
+                  [0,1,1],
+                  [1,0,1],
+                  [1,1,1]])
+    y = np.array([[0],[1],[1],[0]])
+    nn = NeuralNetwork(X,y)
+
+    for i in range(2000):
+        nn.feedforward()
+        w1, w2 = nn.backprop()
+
+    print(nn.output)
---
+++ Project 3/mnist_2.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 10:56:20 2019
+
+@author: daksh
+"""
+
+# Command for runnning via IPython console in Spyder:
+# runfile('mnist_2.py', args = '500')
+# arguments = number of epochs to train for
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+""" For testing only start"""
+#num_epochs = 500
+""" For testing only end"""
+
+# Input arguments:
+num_epochs = int(sys.argv[1])
+
+X = np.load('images_flat.npy')
+y = np.load('labels.npy')
+X = X / 255
+
+y_new = np.zeros(y.shape)
+y_new[np.where(y == 0.0)[0]] = 1
+y = y_new
+
+m = 60000
+m_test = X.shape[0] - m
+X_train = X[:m].T
+X_test = X[m:].T
+y_train = y[:m].reshape(1,m)
+y_test = y[m:].reshape(1,m_test)
+
+np.random.seed(138)
+shuffle_index = np.random.permutation(m)
+X_train, y_train = X_train[:,shuffle_index], y_train[:,shuffle_index]
+
+def sigmoid(z):
+    s = 1 / (1 + np.exp(-z))
+    return s
+
+def compute_loss(Y, Y_hat):
+    m = Y.shape[1]
+    L = -(1./m) * ( np.sum( np.multiply(np.log(Y_hat),Y) ) + np.sum( np.multiply(np.log(1-Y_hat),(1-Y)) ) )
+
+    return L
+
+learning_rate = 1
+
+X = X_train
+Y = y_train
+
+n_x = X.shape[0]
+m = X.shape[1]
+
+W = np.random.randn(n_x, 1) * 0.01
+b = np.zeros((1, 1))
+
+print('Start Training')
+
+for i in range(num_epochs):
+    
+    Z = np.matmul(W.T, X) + b
+    A = sigmoid(Z)
+
+    cost = compute_loss(Y, A)
+
+    dW = (1/m) * np.matmul(X, (A-Y).T)
+    db = (1/m) * np.sum(A-Y, axis=1, keepdims=True)
+
+    W = W - learning_rate * dW
+    b = b - learning_rate * db
+
+    if (i % 100 == 0):
+        print("Epoch", i, "cost: ", cost)
+
+print("Final cost:", cost)
+
+Z = np.matmul(W.T, X_test) + b
+A = sigmoid(Z)
+
+#i = 3
+#plt.imshow(X_train[:,i].reshape(28,28), cmap = matplotlib.cm.binary)
+#plt.axis("off")
+#plt.show()
+
+idx_0 = np.where(y_test == 1)
+
+Accuracy = np.sum(1 - (y_test[idx_0] - A[idx_0]))/A[idx_0].shape[0]
+s = 'Accuracy = ' + np.str(np.around(Accuracy, decimals = 4)*100) + '%'
+
+plt.plot(np.arange(A[idx_0].shape[0]), A[idx_0], 'b.')
+plt.plot(np.arange(A[idx_0].shape[0]), y_test[idx_0], 'r.')
+plt.xlabel('Data Points')
+plt.ylabel('Class 0 or 1')
+plt.rcParams.update({'font.size': 12})
+plt.legend(['Predicted Class', 'True Class'])
+plt.title('ANN Predicted vs True Class for number \'0\'')
+plt.ylim([0, 1.5])
+plt.text(10, 1.25, s, fontsize=12)
+plt.show()
+
+
